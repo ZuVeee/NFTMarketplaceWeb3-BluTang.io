f3e6fd4e83547cb435a022d3033de0173535ad29
Dev ZuVeee <zuveeedev@MacBookPros-MBP.lan>
2022-12-19 06:22:35 -0500

    final

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <ThirdwebProvider desiredChainId={ChainId.Polygon}
+      chainRpc={{[ChainId.Mumbai]:'https://polygon-mumbai.infura.io/v3/ed8a810bcf80436c99aaaaff75abad52'}}
+    >
+      <ThemeProvider enableSystem={true} attribute='class'>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ThirdwebProvider>
+  )
 }
+
+export default MyApp
+
---
+++ static/collections.js
@@ -0,0 +1,26 @@
+export const doodlesCollection = {
+    name: 'BluTang',
+    creator: 'BluTang.io',
+    description:
+      'A community-driven collectibles project featuring art of life underwater. ',
+    isVerified: true,
+    slug: 'blutang-official',
+    banner_image_url:
+      'https://lh3.googleusercontent.com/svc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw=s2500',
+    featured_image_url:
+      'https://lh3.googleusercontent.com/svc_rQkHVGf3aMI14v3pN-ZTI7uDRwN-QayvixX-nHSMZBgb1L1LReSg1-rXj4gNLJgAB0-yD8ERoT-Q2Gu4cy5AuSg-RdHF9bOxFDw=s300',
+    image_url:
+      'https://lh3.googleusercontent.com/7B0qai02OdHA8P_EOVK672qUliyjQdQDGNrACxs7WnTgZAkJa_wWURnIFKeOh5VTf8cfTqW3wQpozGedaC9mteKphEOtztls02RlWQ=s120',
+    discord_url: '',
+    external_url: '',
+    twitter_username: '',
+    instagram_username: null,
+    stats: {
+      total_volume: 119183.29519312919,
+      total_supply: 10000,
+      num_owners: 4897,
+      floor_price: 22.6,
+    },
+    address: '0x4fe4a11A71d821fD7347e95184cF0c708F7273a3',
+  }
+  
